ee9699fcb3cdbe00fafb88413be9cdfebcd338d5
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2021-12-24 13:28:05 -0300

    Circuit Breaker com Polly

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -23,7 +23,6 @@ namespace NSE.Catalogo.API.Controllers
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
-            throw new Exception("Erro");
             return await _produtoRepository.ObterTodos();
         }
 
--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -6,7 +6,9 @@ using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
 using Polly;
 using Polly.Extensions.Http;
+using Polly.Retry;
 using System;
+using System.Net.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -18,27 +20,20 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            var retryWaitPolicy = HttpPolicyExtensions
-                .HandleTransientHttpError()
-                .WaitAndRetryAsync(new []
-                {
-                    TimeSpan.FromSeconds(1),
-                    TimeSpan.FromSeconds(5),
-                    TimeSpan.FromSeconds(10)
-                }, 
-                (outcome, timespan, retryCount, context) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine($"Tentando pela {retryCount} vez!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                });
-
             services.AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-                .AddPolicyHandler(retryWaitPolicy);
-                //.AddTransientHttpErrorPolicy(
-                //    p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AddTransientHttpErrorPolicy(
+                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
+            // Número máximo de tentativas = 5
+            // Quando esse número for alcançado, a aplicação espera 30 segundos antes de realizar uma nova tentativa
+            // É uma máquina de estado que vale para todas as conexões
+            // Se 5 usuários tiverem 1 erro cada, a aplicação entra em Circuit Breaker
+            // 5 tentativas consecutivas, caso alguma ocorra com sucesso, a máquina de estado é "resetada"
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
 
             //services.AddHttpClient("Refit", options =>
             //    {
@@ -46,9 +41,29 @@ namespace NSE.WebApp.MVC.Configuration
             //    })
             //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
             //    .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+        }
+    }
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddScoped<IUser, AspNetUser>();
+    public class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                },
+                (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
         }
     }
 }
