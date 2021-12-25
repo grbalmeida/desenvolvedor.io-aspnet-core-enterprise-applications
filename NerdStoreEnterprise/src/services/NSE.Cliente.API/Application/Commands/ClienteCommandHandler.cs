d85f1fa1ec438c0ea45d112b6d06e8147cf30aec
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2021-12-25 07:24:38 -0300

    MediatR e Command Handler

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,12 +1,30 @@
-﻿namespace NSE.Clientes.API.Application.Commands
-{
-    public class ClienteCommandHandler
-    {
-        public void Manipular(RegistrarClienteCommand message)
-        {
-            // Validar comando
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
 
-            // Persistir na base
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+            // Validações de negócio
+
+            // Persistir no banco!
+            if (true) // Já existe cliente com o CPF informado
+            {
+                AdicionarErro("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            return message.ValidationResult;
         }
     }
 }
