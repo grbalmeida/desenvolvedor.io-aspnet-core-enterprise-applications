69789b1671aa6d6ae9ec521175b547c3af5c2562
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2021-12-25 06:59:06 -0300

    Command e Command Handler

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // Validar comando
+
+            // Persistir na base
+        }
+    }
+}
