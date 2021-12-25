fbbdd7097038d104956c25fbef3004537a1fecac
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2021-12-24 22:25:26 -0300

    Mapeamento das tabelas

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Migrations/20211225012148_AlteracaoNomeColunaCpf.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NSE.Clientes.API.Migrations
+{
+    public partial class AlteracaoNomeColunaCpf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cpf_Numero",
+                table: "Clientes",
+                newName: "Cpf");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Clientes",
+                newName: "Cpf_Numero");
+        }
+    }
+}
