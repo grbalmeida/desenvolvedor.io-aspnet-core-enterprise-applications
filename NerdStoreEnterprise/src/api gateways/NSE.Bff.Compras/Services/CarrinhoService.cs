382a6fb173685d1fe40724094f10a9fb40cf03f9
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-01-21 13:53:13 -0300

    Implementando o BFF de Compras

---
+++ NerdStoreEnterprise/src/api gateways/NSE.Bff.Compras/Services/CarrinhoService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.Bff.Compras.Extensions;
+using NSE.WebApp.MVC.Services;
+using System;
+using System.Net.Http;
+
+namespace NSE.Bff.Compras.Services
+{
+    public interface ICarrinhoService
+    {
+
+    }
+
+    public class CarrinhoService : Service, ICarrinhoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CarrinhoService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.CarrinhoUrl);
+        }
+    }
+}
