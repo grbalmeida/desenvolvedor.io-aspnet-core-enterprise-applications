e1b8c7c500bb9b4cf5d11b2b5137eb229b2b04d8
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-01-23 16:30:19 -0300

    Application Query Stack e Specification Pattern

--- NerdStoreEnterprise/src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
+++ NerdStoreEnterprise/src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
@@ -1,4 +1,5 @@
 ﻿using NSE.Core.DomainObjects;
+using NSE.Pedidos.Domain.Specs;
 using System;
 
 namespace NSE.Pedidos.Domain
@@ -15,5 +16,20 @@ namespace NSE.Pedidos.Domain
         public DateTime DataValidade { get; private set; }
         public bool Ativo { get; private set; }
         public bool Utilizado { get; private set; }
+
+        public bool EstaValidoParaUtilizacao()
+        {
+            return new VoucherAtivoSpecification()
+                .And(new VoucherDataSpecification())
+                .And(new VoucherQuantidadeSpecification())
+                .IsSatisfiedBy(this);
+        }
+
+        public void MarcarComoUtilizado()
+        {
+            Ativo = false;
+            Utilizado = true;
+            Quantidade = 0;
+        }
     }
 }
