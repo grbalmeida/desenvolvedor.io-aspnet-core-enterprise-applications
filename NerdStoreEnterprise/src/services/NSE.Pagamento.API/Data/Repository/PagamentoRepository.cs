ac648bf1a493f9a0acc86ce1a5a9d23abd0c86c7
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-03-08 19:58:54 -0300

    Modelagem das classes de pagamento

---
+++ NerdStoreEnterprise/src/services/NSE.Pagamento.API/Data/Repository/PagamentoRepository.cs
@@ -0,0 +1,27 @@
+﻿using NSE.Core.Data;
+using NSE.Pagamentos.API.Models;
+
+namespace NSE.Pagamentos.API.Data.Repository
+{
+    public class PagamentoRepository : IPagamentoRepository
+    {
+        private readonly PagamentosContext _context;
+
+        public PagamentoRepository(PagamentosContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => throw new System.NotImplementedException();
+
+        public void AdicionarPagamento(Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
